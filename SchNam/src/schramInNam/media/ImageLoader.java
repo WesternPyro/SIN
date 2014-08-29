d8790369ad0ff232e70ab0c8afad4e2256307d62
WesternPyro <shadowblade7204@gmail.com>
2014-08-29 14:00:37 -0500

    Removed ignorance of /bin/

--- SchNam/src/schramInNam/media/ImageLoader.java
+++ SchNam/src/schramInNam/media/ImageLoader.java
@@ -2,10 +2,8 @@ package schramInNam.media;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
+import java.awt.Color;
 import javax.imageio.ImageIO;
-
-import com.sun.prism.paint.Color;
 
 public class ImageLoader {
 	
@@ -27,17 +25,5 @@ public class ImageLoader {
 		try {
 			items[0] = ImageIO.read(ImageLoader.class.getResource("/resources/images/weapons/realistic/AK-47.png"));
 		} catch (IOException e){System.out.println(e);}
-		
-		BufferedImage fixed = new BufferedImage(items[0].getWidth(),items[0].getHeight(),BufferedImage.TYPE_INT_RGB);
-		for (int x = 0; x < fixed.getWidth(); x++) {
-			for (int y = 0; y < fixed.getHeight(); y++) {
-				if (items[0].getRGB(x,y) != Color.WHITE.getIntArgbPre()) {
-					fixed.setRGB(x,y,items[0].getRGB(x,y));
-				}
-			}
-		}
-		
-		items[0] = fixed;
 	}
-
 }
