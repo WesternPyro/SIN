abb6d306fd4dcc6c1c506cad461c5a365d8fb3bd
CryHavoc21 <cryhavoc24@gmail.com>
2014-09-01 16:32:04 -0500

    Added testing for mouse input
    which is pretty cool that a component can detect a mouse

--- SchNam/src/schramInNam/util/GameFrame.java
+++ SchNam/src/schramInNam/util/GameFrame.java
@@ -106,6 +106,8 @@ public class GameFrame extends JFrame implements Updatable {
 	
 	public void update() {
 		currentScreen.update();
+		
+		System.out.println("X= " + mouse.getX() + " Y= " + mouse.getY());
 	}
 	
 	/*
